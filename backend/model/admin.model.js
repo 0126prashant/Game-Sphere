c0f1d10a2a5d779465fb0de340d2297c6ce4e7ac
Prashant_Som <prashantsom75@gmail.com>
2023-07-19 23:35:50 +0530

    Alittle code of admin for backenc

--- backend/model/admin.model.js
+++ backend/model/admin.model.js
@@ -1,1 +1,18 @@
-// Write your code here
+
+const mongoose=require("mongoose")
+
+const adminSchema=mongoose.Schema({
+    name:String,
+    email:String,
+    phone:Number,
+    password:String
+},{
+    
+    versionKey:false
+})
+
+const AdminUserModel=mongoose.model("admin", adminSchema)
+
+module.exports={
+    AdminUserModel
+}
