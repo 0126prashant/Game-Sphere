04cfd591e5bf80b4c1ebcd3914d21143fc169194
neo1710 <neeraj3dworld@gmail.com>
2023-07-21 21:26:54 +0530

    day 4

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,10 +7,18 @@ const productRoute=Router();
 
 
 productRoute.get('/products',async (req,res)=>{
-
+const {type}=req.query;
     try {
+        if(type=='games'){
     const product =await productModel.find();
-    res.status(200).send({product}); 
+    res.status(200).send({product});
+        }else if(type=='movies'){
+            const product =await productModel.find();
+    res.status(200).send({product});
+        }else{
+            const product =await productModel.find();
+    res.status(200).send({product});
+        }
 } catch (error) {
     res.status(500).send({msg:'error'})
 }
