b1579cb3cf1880c0858576fc73ce40b6759547b1
Itsmearun1 <112754597+Itsmearun1@users.noreply.github.com>
2023-07-22 20:50:19 +0530

    arun-day5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,12 @@
 import AdminNavbar from './pages/Admin/AdminNavbar';
 import ProductList from './pages/Admin/Productlist';
 import AllRoutes from './components/AllRoutes';
-
-
-
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-    <AdminNavbar/>
-    <ProductList/>
-      <AllRoutes/>
+      <Navbar/>
+      <AllRoutes />
     </div>
   );
 }
